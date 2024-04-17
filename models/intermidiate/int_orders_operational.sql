e18b640c69937d554c9ca5a72a761f328b85e8d3
Antonio Costantini <antliawealth2hub@gmail.com>
2024-04-17 13:41:51 +0000

    commit

--- models/intermidiate/int_orders_operational.sql
+++ models/intermidiate/int_orders_operational.sql
@@ -1,15 +1,16 @@
 SELECT
-  o.orders_id
-  ,o.date_date
-  ,ROUND(o.margin + s.shipping_fee - (s.logcost + s.ship_cost),2) AS operational_margin 
-  ,o.quantity
-  ,o.revenue
-  ,o.purchase_cost
-  ,o.margin
-  ,s.shipping_fee
-  ,s.logcost
-  ,s.ship_cost
+    o.orders_id,
+    o.date_date,
+    ROUND(o.margin + CAST(s.shipping_fee AS FLOAT64) - (CAST(s.logcost AS FLOAT64) + CAST(s.ship_cost AS FLOAT64)), 2) AS operational_margin,
+    o.quantity,
+    o.revenue,
+    o.purchase_cost,
+    o.margin,
+    s.shipping_fee,
+    s.logcost,
+    s.ship_cost
 FROM {{ref("int_orders_margin")}} o
-LEFT JOIN {{ref("stg_gz_raw_data__raw_gz_ship")}} s 
-  USING(orders_id)
-ORDER BY orders_id desc
+LEFT JOIN {{ ref('stg_gz_raw_data__raw_gz_ship') }} s 
+    USING (orders_id)
+ORDER BY 
+    orders_id DESC
--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -1,16 +1,16 @@
 SELECT
-    date_date
-    ,COUNT(orders_id) AS nb_transactions
-    ,ROUND(SUM(revenue),0) AS revenue 
-    ,ROUND(AVG(revenue),1) AS average_basket
-    ,ROUND(SUM(revenue)/COUNT(orders_id),1) AS average_basket_bis
-    ,ROUND(SUM(margin),0) AS margin 
-    ,ROUND(SUM(operational_margin),0) AS operational_margin 
-    ,ROUND(SUM(purchase_cost),0) AS purchase_cost 
-    ,ROUND(SUM(shipping_fee),0) AS shipping_fee 
-    ,ROUND(SUM(logcost),0) AS logcost 
-    ,ROUND(SUM(ship_cost),0) AS ship_cost 
-    ,SUM(quantity) AS quantity 
-FROM {{ref("int_orders_operational")}}
-GROUP BY  date_date
-ORDER BY  date_date DESC
+    date_date,
+    COUNT(orders_id) AS nb_transactions,
+    ROUND(SUM(CAST(revenue AS FLOAT64)), 0) AS revenue,
+    ROUND(AVG(CAST(revenue AS FLOAT64)), 1) AS average_basket,
+    ROUND(SUM(CAST(revenue AS FLOAT64)) / COUNT(orders_id), 1) AS average_basket_bis,
+    ROUND(SUM(CAST(margin AS FLOAT64)), 0) AS margin,
+    ROUND(SUM(CAST(operational_margin AS FLOAT64)), 0) AS operational_margin,
+    ROUND(SUM(CAST(purchase_cost AS FLOAT64)), 0) AS purchase_cost,
+    ROUND(SUM(CAST(shipping_fee AS FLOAT64)), 0) AS shipping_fee,
+    ROUND(SUM(CAST(logcost AS FLOAT64)), 0) AS logcost,
+    ROUND(SUM(CAST(ship_cost AS FLOAT64)), 0) AS ship_cost,
+    SUM(CAST(quantity AS INT64)) AS total_quantity
+FROM `proud-archery-418309`.`dbt_acostantini`.`int_orders_operational`
+GROUP BY date_date
+ORDER BY date_date DESC
